58457f55c8065a0f8558a0cbdfdec38fcfc7b301
Kyrylooo <146863438+Kyrylooo@users.noreply.github.com>
2024-03-18 11:18:43 +0200

    зробив колекцію

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://23agki23:iSz96uTq89jEsn6V@cluster0.bccvqe2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://23agki:TmRqcdQpNqoq5LTX@cluster0.bccvqe2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -8,16 +8,24 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+const database = client.db("costomer");
+const collection = database.collection("test1");
 async function run() {
-  try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    
+    await collection.insertOne({"test key": "test value"});
+    console.log("You successfully connected to MongoDB!");
 }
+
+async function get_test_query() {
+  docment = await collection.find()
+  console.log(docment)
+
+}
+
 run().catch(console.dir);
+
+get_test_query().catch(console.dir);
+
